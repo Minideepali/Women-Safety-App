93d16d27a8a72cc33906cdf450ec6e08a68219fa
CodingStuff <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-03-22 16:19:44 +0530

    Added activities for all buttons

--- WomenSafetyApp/app/src/main/java/com/example/womensafetyapp/MainActivity.java
+++ WomenSafetyApp/app/src/main/java/com/example/womensafetyapp/MainActivity.java
@@ -1,29 +1,53 @@
 package com.example.womensafetyapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.womensafetyapp.UtilsService.SharedPreferenceClass;
 
 public class MainActivity extends AppCompatActivity {
     Button Logout;
+    Button sosButton;
+    Button sirenButton;
+    Button voiceRecordingButton;
+    Button helplineButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Logout = findViewById(R.id.logout);
+        sosButton = findViewById(R.id.sosButton);
+        sirenButton = findViewById(R.id.sirenButton);
+        voiceRecordingButton = findViewById(R.id.voiceRecordingButton);
+        helplineButton = findViewById(R.id.helplineButton);
+
         SharedPreferenceClass sharedPreferenceClass = new SharedPreferenceClass(this);
+
         Logout.setOnClickListener(view -> {
             sharedPreferenceClass.clear();
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
             finish();
+        });
+
+        sosButton.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this, SOSActivity.class));
+        });
+
+        sirenButton.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this, SirenActivity.class));
+        });
+
+        voiceRecordingButton.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this, VoiceRecordingActivity.class));
+        });
+
+        helplineButton.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this, HelplineActivity.class));
         });
     }
 }
