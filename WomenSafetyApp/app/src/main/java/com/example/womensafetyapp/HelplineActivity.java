a0af2a2b97d92bcd70e854d094c3ae6c6f0217b5
CodingStuff-Github <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-04-06 00:27:35 +0530

    Added Call functionality in Helpline Activity
    Also Handles Permission for using call functionality

--- WomenSafetyApp/app/src/main/java/com/example/womensafetyapp/HelplineActivity.java
+++ WomenSafetyApp/app/src/main/java/com/example/womensafetyapp/HelplineActivity.java
@@ -1,10 +1,15 @@
 package com.example.womensafetyapp;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.womensafetyapp.HelplineListView.NumbersView;
 import com.example.womensafetyapp.HelplineListView.NumbersViewAdapter;
@@ -30,9 +35,22 @@ public class HelplineActivity extends AppCompatActivity {
         ListView numbersListView = findViewById(R.id.listView);
         numbersListView.setAdapter(numbersArrayAdapter);
         numbersListView.setOnItemClickListener((parent, view, position, id) -> {
-            String selectedItem = (String) parent.getItemAtPosition(position);
             String selectedNumber = arrayList.get(position).getEmergency_contact_number();
+            //Intent for calling selected number
+            Intent intent = new Intent(Intent.ACTION_CALL);
             Toast.makeText(this, selectedNumber, Toast.LENGTH_SHORT).show();
+            intent.setData(android.net.Uri.parse("tel:" + selectedNumber));
+            //startActivity(intent);
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+            } else {
+
+                try {
+                    startActivity(intent);
+                } catch (SecurityException e) {
+                    e.printStackTrace();
+                }
+            }
         });
     }
 }
