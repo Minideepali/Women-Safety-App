bc4b49aede7c5b9043efdac9c10b16aa0ecdee83
CodingStuff-Github <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-04-21 08:13:30 +0530

    New Files

---
+++ WomenSafetyApp/app/src/main/java/com/example/womensafetyapp/UserInfo.java
@@ -0,0 +1,13 @@
+package com.example.womensafetyapp;
+
+public class UserInfo {
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
